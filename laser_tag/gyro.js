71639641f74e7b512f8635cdc93b24de75d2a272
Shishir Bhat <shishirvbhat@gmail.com>
2017-10-14 02:32:42 -0400

    initial commit

--- laser_tag/gyro.js
+++ laser_tag/gyro.js
@@ -4,6 +4,9 @@
     var gn = new GyroNorm();
     var a = 0;
     var b = 0;
+    var lat = 0;
+    var long = 0;
+    var xhr = new XMLHttpRequest(); // use this later
 
     gn.init().then(function(){
       gn.start(function(data){
@@ -14,5 +17,24 @@
     }).catch(function(e){
       // Error if the browser doesn't support device orientation
     });
+
+    function buttonClick() {
+        getLocation();
+        //TODO: Send location and orientation to server
+    }
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPostion(setPosition);
+        } else {
+            alert("Geolocation services not suppported by this browser version");
+        }
+    }
+
+    function setPosition(position) {
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
+    }
+
 
 </script>
